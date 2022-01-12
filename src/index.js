c613560e4771a8d42bd8a92614cd4d0701cf55b4
Carlos Aviña <juan.avina@wizeline.com>
2022-01-11 18:50:27 -0600

    Move index to src

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import { Client, Intents } from "discord.js";
 
 dotenv.config();
@@ -6,8 +6,9 @@ dotenv.config();
 const token = process.env.DISCORD_TOKEN;
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-client.once("ready", () => {
-  console.log("Creo en la suerte");
-});
-
 client.login(token);
+
+client.once("ready", () => {
+  const channel = client.channels.cache.get("929571416158908436");
+  channel.send("¿Crees en la suerte?");
+});
