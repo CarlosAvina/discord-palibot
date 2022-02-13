3192acd1d5deb8c8b57f901e572d4e3c8c61e0dd
Carlos Aviña <juan.avina@wizeline.com>
2022-02-13 15:03:50 -0600

    Create minecraft expiry command

--- src/commands/minecraft.js
+++ src/commands/minecraft.js
@@ -1,9 +1,15 @@
+import { MessageEmbed } from "discord.js";
 import { SlashCommandBuilder, userMention, bold } from "@discordjs/builders";
 import { initializeApp, cert } from "firebase-admin/app";
 import { getFirestore, Timestamp } from "firebase-admin/firestore";
 import creds from "../../service-account-file.js";
 
-initializeApp({ credential: cert(creds) });
+import returnEmbedColor from "../utils/expiryEmbedColor.js";
+import returnEmbedDescription from "../utils/expiryEmbedDescription.js";
+
+initializeApp({
+  credential: cert(creds),
+});
 
 const db = getFirestore();
 
@@ -34,6 +40,11 @@ const status = {
   description: "Days without incidents",
 };
 
+const expiry = {
+  name: "expiry",
+  description: "Dice cuantos dias quedan del server con mr. increible :D",
+};
+
 const minecraft = {
   data: new SlashCommandBuilder()
     .setName(command.name)
@@ -60,6 +71,9 @@ const minecraft = {
     )
     .addSubcommand((subcommand) =>
       subcommand.setName(status.name).setDescription(status.description)
+    )
+    .addSubcommand((subcommand) =>
+      subcommand.setName(expiry.name).setDescription(expiry.description)
     ),
   async execute(interaction) {
     const { commandName, options } = interaction;
@@ -134,6 +148,53 @@ const minecraft = {
           await interaction.reply(`${emojiNumbers} dias sin incidentes :D`);
         } else {
           await interaction.reply(`Aún no hay incidentes :D`);
+        }
+      }
+      if (options.getSubcommand() === expiry.name) {
+        const snapshot = await db
+          .collection("expiry")
+          .limit(1)
+          .get()
+          .catch((err) => console.error(err));
+
+        if (!snapshot.empty) {
+          const { start, expiry } = snapshot.docs[0].data();
+
+          const currentDate = new Date();
+          const startDate = start.toDate();
+          const expiryDate = expiry.toDate();
+
+          const numOfImages = 18;
+
+          const timeDifference = expiryDate.getTime() - startDate.getTime();
+          const daysAvailable = Math.floor(
+            timeDifference / (1000 * 60 * 60 * 24)
+          );
+
+          const chunks = daysAvailable / numOfImages;
+
+          const timeRemaining = expiryDate.getTime() - currentDate.getTime();
+          const daysRemaining = Math.floor(
+            timeRemaining / (1000 * 60 * 60 * 24)
+          );
+
+          const place = numOfImages - Math.ceil(daysRemaining / chunks) + 1;
+          const numString = String(place).padStart(2, "0");
+
+          const url = `${process.env.STORAGE_URL}/o/mrincredible_${numString}.png?alt=media`;
+          const emojiNumbers = numberToEmoji(daysRemaining);
+
+          const exampleEmbed = new MessageEmbed()
+            .setColor(returnEmbedColor(place))
+            .setTitle(
+              `Quedan ${emojiNumbers} ${
+                daysRemaining === 1 ? "día" : "días"
+              } para la destrucción del server de minecraft.`
+            )
+            .setDescription(returnEmbedDescription(place))
+            .setImage(url);
+
+          await interaction.reply({ embeds: [exampleEmbed] });
         }
       }
     }
